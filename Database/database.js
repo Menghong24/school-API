fdee689847755fc740f4c51bc818ad7795095dba
menghongmydarling@gmail.com <your-email@example.com>
2026-06-09 11:16:26 +0700

    edit port

--- Database/database.js
+++ Database/database.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose");
 
 async function connectDatabase() {
     try {
-        await mongoose.connect('mongodb+srv://boymenghongxxx_db_user:Admin123@cluster0.chyzbjh.mongodb.net/?appName=Cluster0')
+        await mongoose.connect(process.env.PORT)
         console.log('Database connected successfully');    
     } catch (error){
         console.log('Error conecting to the database:',error)
